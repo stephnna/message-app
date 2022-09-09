f426391e36410c355b75b5f9bb4c20531bff96da
stephnna <stephennnabuike@gmail.com>
2022-09-09 19:45:19 +0100

    Created update index.html, and created class components

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,63 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuidV4} from "uuid";
+// create an interfact that holds the content of a user
+interface User {
+sendMessage: () => string;
+}
 
-import confetti from 'canvas-confetti';
+type userDetail = {
+  id: string,
+  message: string,
+  firstName: string,
+  lastName: string,
+  email: string, 
+  createdAt: Date, 
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+      //  parent as a type of user implements user
+class Parent implements User {
+  public constructor(     
+    protected readonly email: string,
+    protected readonly photo?: String,
+    protected parent = document.getElementById('parent-button'),
+    protected messageArray: [] = [],
+    ) {}
+
+    public sendMessage(): string {
+    return this.email;
+  }
+
+ public renderParentMessage() {
+  parent.onclick = () => {
+      const parentMessage = document.getElementById('parent-input') as HTMLInputElement;
+      const firstName = document.getElementById('parent-input') as HTMLInputElement;
+      const lastName = document.getElementById('parent-input') as HTMLInputElement;
+      const email = document.getElementById('parent-input') as HTMLInputElement;
+        
+      if (parentMessage?.value === '' || parentMessage?.value == null) return;    
+
+      const newMessage = {        
+        id: uuidV4(),
+        message: parentMessage.value,
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value, 
+        createdAt: new Date,       
+         };
+         this.messageArray.push(Array<userDetail>(newMessage));      
+    };
+  }
+}
+       // Teacher having the same property as parent extends parent 
+class Teacher extends Parent {
+  public constructor(
+    id: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    photo?: String,
+  ) {
+    super(id, firstName, lastName, email, photo);
+  }  
+}
+
+// const user = new Parent<>(20);
